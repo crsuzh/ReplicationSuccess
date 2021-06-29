824282f09953c60e8d45cbab6ed74b1b51bd886d
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-06-29 13:28:55 +0200

    checks for effectSizeReplicationSuccess

--- R/effectSizeReplicationSuccess.R
+++ R/effectSizeReplicationSuccess.R
@@ -1,9 +1,53 @@
+.effectSizeReplicationSuccess_ <- function(zo, 
+                                           c = 1, 
+                                           level = 0.025, 
+                                           alternative = c("one.sided", "two.sided"),
+                                           type = c("golden", "nominal", "liberal", "controlled")){
+    
+    stopifnot(is.numeric(zo),
+              length(zo)==1,
+              is.finite(zo),
+                            
+              is.numeric(c),
+              length(c)==1,
+              is.finite(c),
+              
+              is.numeric(level),
+              length(level)==1,
+              is.finite(level),
+              0 < level, level < 1,
+              
+              !is.null(alternative))
+    alternative <- match.arg(alternative)
+    
+    stopifnot(!is.null(type))
+    type <- match.arg(type)
+    
+    alphas <- levelSceptical(level = level, 
+                             alternative = alternative, 
+                             type = type)
+    zalphas <- p2z(alphas, alternative = alternative)
+    K <- zo^2 / zalphas^2
+    
+    if (zalphas > zo) {
+      warning(paste("Replication success is not achievable at this level as",
+                    zo, " < ", round(p2z(levelSceptical(level = level,
+                                                        alternative = alternative,
+                                                        type = type)), 3)))
+      d <- NA
+    } else {
+        d <- if(c < Inf) sqrt(1 + c/(K - 1))/(sqrt(K * c)) else 1/sqrt(K * (K - 1))
+    }
+    
+    return(d)
+}
+
 #' Computes the minimum relative effect size to achieve replication success
 #'
 #' The minimum relative effect size (replication to original) to achieve replication success
 #' is computed based on the result of the original study and the corresponding variance ratio (relative sample size).
 #' 
-#' @param zo  Numeric vector of z-values from original studies. 
+#' @param zo Numeric vector of z-values from original studies. 
 #' @param c Numeric vector of variance ratios of the original and replication effect estimates.
 #' This is usually the ratio of the sample size of the replication study to the sample
 #' size of the original study.
@@ -11,7 +55,7 @@
 #' @param alternative Specifies if the replication success level is "one.sided" (default) or "two.sided".
 #' If the significance level is one-sided, then effect size calculations are based on a one-sided assessment of
 #' replication success in the direction of the original effect estimate.
-#' @param type  Type of recalibration. Can be either "golden" (default), "nominal" (no recalibration),
+#' @param type Type of recalibration. Can be either "golden" (default), "nominal" (no recalibration),
 #' "liberal", or "controlled". "golden" ensures that for an original study just significant at
 #' the specified \code{level}, replication success is only possible for replication effect estimates larger than the original one.
 #' See \code{\link{levelSceptical}} for details about recalibration types.
@@ -35,34 +79,4 @@
 #' effectSizeReplicationSuccess(zo = zo, c = 2, level = 0.05,
 #'                              alternative = "two.sided", type = "nominal")
 #' @export
-effectSizeReplicationSuccess <- function(zo, 
-                                         c = 1, 
-                                         level = 0.025, 
-                                         alternative = "one.sided",
-                                         type = "golden"){
-  
-  mRV <- mapply(FUN = function(zo, c, level){
-    if (!is.numeric(level) || (level <= 0 || level >= 1)) 
-      stop("level must be numeric and in (0,1)!")
-    
-    alphas <- levelSceptical(level = level, 
-                             alternative = alternative, 
-                             type = type)
-    zalphas <- p2z(alphas, alternative = alternative)
-    K <- zo^2/zalphas^2
-    
-    if (zalphas > zo) {
-      warning(paste("Replication success is not achievable at this level as",
-                    zo, " < ", round(p2z(levelSceptical(level = level,
-                                                            alternative = alternative,
-                                                            type = type)),
-                                     3)))
-      d <- NA
-    } else {
-    d <- ifelse(c < Inf, sqrt(1 + c/(K - 1))/(sqrt(K * c)), 1/sqrt(K * (K - 1)))
-    }
-    return(d)
-  }, zo, c, level)
-  
-  return(mRV)
-}
+effectSizeReplicationSuccess <- Vectorize(.effectSizeReplicationSuccess_)
