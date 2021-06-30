5b3bdacf5d8b90e74462fc9300e3428731ced853
Florian Gerber <flora.fauna.gerber@gmail.com>
2021-06-30 13:49:05 +0200

    input tests for levelEquivalent()

--- scratch/lookAround.R
+++ scratch/lookAround.R
@@ -2,3 +2,10 @@ try(detach("package:ReplicationSuccess", unload=TRUE), silent=TRUE)
 system("make -C .. lib")
 library("ReplicationSuccess", lib.loc = "../lib/")
 
+levelEquivalent(dinf = 0.8, level = 0.025)
+levelEquivalent(dinf = 0.8, level = 0.05, alternative="two.sided")
+
+levelEquivalent(dinf = c(0.8, .9), level = 0.025)
+levelEquivalent(dinf = 0.8, level = c(0.05, .002), alternative="two.sided")
+levelEquivalent(dinf = c(0.8,.9), level = c(0.05, .002), alternative="two.sided")
+
