c8d97808ee92159f24b27ec923aee8be6188e6a0
SamCH93 <samuel.pawel@gmail.com>
2021-07-08 11:39:32 +0200

    Wording documentation powerSignificance

--- R/powerSignificance.R
+++ R/powerSignificance.R
@@ -87,9 +87,9 @@
 #' the contribution of the original study is shrunken towards zero based
 #' on the evidence in the original study (with an empirical Bayes shrinkage estimator).
 #' @param alternative Either "one.sided" (default) or "two.sided".
-#' Specifies if the replication success level is one-sided or two-sided.
-#' If the replication success level is one-sided, then power calculations are based on a
-#' one-sided assessment of replication success in the direction of the
+#' Specifies if the significance level is one-sided or two-sided.
+#' If the significance level is one-sided, then power calculations are based on a
+#' one-sided assessment of significance in the direction of the
 #' original effect estimates.
 #' @param h The relative between-study heterogeneity, i.e., the ratio of the heterogeneity
 #' variance to the variance of the original effect estimate.
