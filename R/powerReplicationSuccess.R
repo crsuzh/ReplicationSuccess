eea7c7a17cd4acb38e95a1b479090f59f63fa2c8
Charlotte <charlotte.micheloud@uzh.ch>
2022-08-19 15:45:15 +0200

    more reference updating

--- R/powerReplicationSuccess.R
+++ R/powerReplicationSuccess.R
@@ -155,7 +155,7 @@
 #' Held, L., Micheloud, C., Pawel, S. (2022). The assessment of replication
 #' success based on relative effect size.
 #' \emph{The Annals of Applied Statistics}, \bold{16}, 706-720.
-#'  \url{https://doi.org/10.1214/21-AOAS1502}
+#'  \doi{10.1214/21-AOAS1502}
 #' @seealso \code{\link{sampleSizeReplicationSuccess}}, \code{\link{pSceptical}},
 #' \code{\link{levelSceptical}}
 #' @examples
