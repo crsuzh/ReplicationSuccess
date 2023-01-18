31f6934d41e0c338016f638d2c1bfb7dbfd67beb
Charlotte <charlotte.micheloud@uzh.ch>
2023-01-18 16:37:50 +0100

    FZ back to pSceptical.R

--- R/pSceptical.R
+++ R/pSceptical.R
@@ -1,3 +1,32 @@
+# FZ omputes the cumulative distribution function of z = z_S^2
+
+FZ <- function(z, c) {
+  if (z <= 0) {
+    return(0)
+  } else if (c == 0) {
+    return(1 - 4*(1 - pnorm(sqrt(z)))^2)
+  } else if (c == 1) {
+    return(pgamma(z, 1/2, 2))
+  } else {
+    f <- function(t, c, z) {
+      t1 <- exp(-(c - 1)*z/(sqrt(1 + (c - 1)*t) - 1))
+      t2 <- 1 / sqrt(t*(1 - t))
+      return(t1*t2)
+    }
+    myint <- integrate(
+      f,
+      lower = 0,
+      upper = 1,
+      z = z,
+      c = c
+    )$value
+    result <- 1 - myint/pi
+    return(result)
+  }
+}
+
+
+
 #' @export
 .pSceptical_ <- function(zo,
                          zr,
