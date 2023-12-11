0bda610ec1080cb584b25efe8699360d14913975
Felix Hofmann <felix_hofmann_13@hotmail.com>
2023-12-11 09:11:48 +0100

    Add data sets and first draft of docs

--- data-raw/RPCB.R
+++ data-raw/RPCB.R
@@ -1,11 +1,148 @@
 # load ReplicationSuccess
 library(ReplicationSuccess)
+library(dplyr) # for data wrangling
 
-# Load RPCB data (from Samuel Pawel's Github)
-RPCB <- read.csv(
-    "https://raw.githubusercontent.com/SamCH93/thesis/main/source/data/rpcb-effect-level.csv"
+## RPCB data with SMD transformed effect sizes but before aggregating internal
+## replications:
+## Download the zip archive if it does not exist
+destfile <- file.path(tempdir(), "osf_squy7.zip")
+if (!file.exists(destfile)) {
+    download.file(
+        url = "https://files.osf.io/v1/resources/squy7/providers/github/Prepped%20data/Intermediate%20work/?zip=",
+        destfile = destfile
+    )
+}
+contents <- unzip(zipfile = destfile, list = TRUE)
+file <- grep(
+    pattern = "intermediate_dataset_step3.csv",
+    x = contents$Name,
+    fixed = TRUE,
+    value = TRUE
 )
+out_dir <- file.path(tempdir(), "RPCB")
+dir.create(out_dir, showWarnings = FALSE)
+unzip(zipfile = destfile, files = file, exdir = out_dir)
+data_file <- file.path(out_dir, file)
 
+# Import data, clean up, save 
+
+dat <- read.csv(file = data_file)
+dat2 <- dat %>%
+    ## only take studies with quantitative effect information available
+    filter(!is.na(origDirection),
+           quantPair == TRUE)
+
+## organize data set for reanalysis
+datClean <- dat2 %>%
+    mutate(
+        ## define identifier for paper, experiment, effect, internal replication
+        id = paste0("(", pID, ", ", eID, ", ", oID, ", ", internalID, ")")
+    ) %>%
+    select(
+        osf = OSF.project.link,
+        id,
+        paper = pID,
+        experiment = eID,
+        effect = oID,
+        internalReplication = internalID,
+        resulto = origDirection,
+        resultr = repDirection,
+        ## effect sizes and standard errors on SMD scale
+        smdo = origES3,
+        so = origSE3,
+        smdr = repES3,
+        sr = repSE3,
+        ## effect sizes, standard errors, p-values, confidence intervals on original scale
+        effectType = Effect.size.type,
+        ESo = Original.effect.size,
+        seESo = Original.standard.error,
+        lowerESo = Original.lower.CI,
+        upperESo = Original.upper.CI,
+        po = origPval,
+        ESr = Replication.effect.size,
+        seESr = Replication.standard.error,
+        lowerESr = Replication.lower.CI,
+        upperESr = Replication.upper.CI,
+        pr = repPval,
+        ## original and replication sample size
+        ## (not clear whether group or full sample size)
+        no = origN,
+        nr = repN
+    )
+
+## should give 20 original null effects and 10 "successful" null replications
+## (see null results in Table 1)
+datClean %>%
+    summarise(nulls = sum(resulto == "Null"),
+              successes = sum(resulto == "Null" &
+                              resultr %in% c("Null-positive", "Null-negative",
+                                             "Null")))
+## this should give the same
+datClean %>%
+    summarise(nulls = sum(resulto == "Null"),
+              successes = sum(resulto == "Null" &
+                              pr > 0.05))
+
+## should give 112 original positive effects and 44 successful replications
+## (see positive results in Table 1)
+datClean %>%
+    summarise(positives = sum(resulto == "Positive"),
+              successes = sum(resulto == "Positive" &
+                              resultr == "Positive" &
+                              sign(smdo) == sign(smdr)))
+## this should give the same
+datClean %>%
+    summarise(positives = sum(resulto == "Positive"),
+              successes = sum(resulto == "Positive" &
+                              pr < 0.05 &
+                              sign(smdo) == sign(smdr)))
+
+## save
+# write.csv(datClean, "rpcb-outcome-level.csv", row.names = FALSE)
+
+## aggregate internal replications with fixed-effect meta-analysis to get
+## "effect-level data"
+dat3 <- datClean %>%
+    mutate(id2 = paste0("(", paper, ", ", experiment, ", ", effect, ")"))
+datClean2 <- lapply(unique(dat3$id2), FUN = function(id2) {
+    ## pool the internal replications with fixed-effect meta-analysis
+    datr <- dat3[dat3$id2 == id2, ]
+    nreplications <- nrow(datr)
+    varsmdrMA <- 1 / sum(1 / datr$sr^2)
+    smdrMA <- sum(datr$smdr / datr$sr^2) * varsmdrMA
+    ## add the sample sizes of the internal replications
+    nrTotal <- sum(datr$nr)
+    if (nreplications > 1) {
+        ## TODO translate back the pooled effect to the original scale?
+        ## check whether still a "null" replication based on Wald p-value
+        prMA <- 2 * pnorm(q = abs(smdrMA) / sqrt(varsmdrMA), lower.tail = FALSE)
+        if (prMA > 0.05) {
+            resultMA <- "Null"
+            prNew <- prMA
+        } else {
+            resultMA <- "Positive"
+            prNew <- prMA
+        }
+    } else {
+        resultMA <- datr$resultr
+        prNew <- datr$pr
+    }
+    out <- datr[1, ] %>%
+        mutate(smdr = smdrMA,
+               sr = sqrt(varsmdrMA),
+               nr = nrTotal,
+               id = id2,
+               resultr = resultMA,
+               pr = prNew) %>%
+        select(-internalReplication, -id2)
+    return(out)
+}) %>%
+    bind_rows()
+
+## should give 15 original null effects and 11 "successful" null replications
+## (see null results in Table 1)
+# Load RPCB data (from Samuel Pawel's Github)
+RPCB <- datClean2
 # Recalculate p-values is done in Charlotte Micheloud's thesis:
 # For code see:
 # https://gitlab.uzh.ch/charlotte.micheloud/phd-thesis/-/blob/master/reproMaterial/Thesis_introduction.R
